bb303925e319f87198376501ab9cfc0d5498bf7e
mainfn <_@mainfn.dev>
2023-09-04 23:01:20 +0900

    feat: OutputView 출력 기능 일부 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,24 @@
+package lotto.view;
+
+public final class OutputView {
+  public void printInputPurchaseAmountMessage() {
+    System.out.println("구입금액을 입력해 주세요.");
+  }
+
+  // TODO: 출력 정보를 담은 인자 필요
+  public void printPurchaseCompletionMessage() {
+    System.out.println();
+  }
+
+  public void printInputWinningNumbersMessage() {
+    System.out.println("당첨 번호를 입력해 주세요.");
+  }
+
+  public void printInputBonusNumbersMessage() {
+    System.out.println("보너스 번호를 입력해 주세요.");
+  }
+
+  // TODO: 출력 정보를 담은 인자 필요
+  public void printResult() {
+  }
+}
