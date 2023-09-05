ba98911d4a643974838222737e7ba612141089ee
mainfn <_@mainfn.dev>
2023-09-05 10:53:32 +0900

    feat: LottoTickets 초안 생성

---
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import java.util.List;
+
+public final class LottoTickets {
+
+  private final List<LottoTicket> lottoTickets;
+
+  public LottoTickets(final List<LottoTicket> lottoTickets) {
+    this.lottoTickets = lottoTickets;
+  }
+}
