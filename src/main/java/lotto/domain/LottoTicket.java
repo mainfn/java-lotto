cead4fa39c9c15c39ae98e8a0415dda3071942bf
mainfn <_@mainfn.dev>
2023-09-05 10:53:02 +0900

    feat: LottoTicket 초안 생성

---
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -0,0 +1,13 @@
+package lotto.domain;
+
+import java.util.List;
+
+public final class LottoTicket {
+
+  private final List<Integer> numbers;
+
+  public LottoTicket(final List<Integer> numbers) {
+    this.numbers = numbers;
+  }
+
+}
