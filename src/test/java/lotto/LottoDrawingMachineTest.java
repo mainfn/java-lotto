91116b8e5e16b2d2f1453e33a7c074ea072196cb
mainfn <_@mainfn.dev>
2023-09-06 00:47:07 +0900

    refactor: 테스트 코드 수정

--- src/test/java/lotto/LottoDrawingMachineTest.java
+++ src/test/java/lotto/LottoDrawingMachineTest.java
@@ -3,7 +3,7 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-import lotto.domain.LottoDrawingMachine;
+import lotto.domain.LottoTicket;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +12,7 @@ class LottoDrawingMachineTest {
   @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
   @Test
   void createLottoByOverSize() {
-    assertThatThrownBy(() -> LottoDrawingMachine.of(List.of(1, 2, 3, 4, 5, 6, 7), 10))
+    assertThatThrownBy(() -> new LottoTicket(List.of(1, 2, 3, 4, 5, 6, 7)))
         .isInstanceOf(IllegalArgumentException.class);
   }
 
@@ -20,7 +20,7 @@ class LottoDrawingMachineTest {
   @Test
   void createLottoByDuplicatedNumber() {
     // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-    assertThatThrownBy(() -> LottoDrawingMachine.of(List.of(1, 2, 3, 4, 5, 5), 10))
+    assertThatThrownBy(() -> new LottoTicket(List.of(1, 2, 3, 4, 5, 5)))
         .isInstanceOf(IllegalArgumentException.class);
   }
 
