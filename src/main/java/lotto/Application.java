e30f52491fa9198bac35f1a3c55f04a3beedf2f8
mainfn <_@mainfn.dev>
2023-09-06 00:44:42 +0900

    feat: Application 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.LottoDrawingMachine;
+import lotto.domain.LottoStore;
+import lotto.domain.RandomNumbersGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
 
   public static void main(final String[] args) {
+    final RandomNumbersGenerator randomNumbersGenerator = new RandomNumbersGenerator(1, 45);
+    final LottoStore lottoStore = new LottoStore(randomNumbersGenerator);
+    final LottoDrawingMachine lottoDrawingMachine = new LottoDrawingMachine(randomNumbersGenerator);
+    final OutputView outputView = new OutputView();
+    final InputView inputView = new InputView(outputView);
+    final LottoController lottoController = new LottoController(inputView, outputView, lottoStore,
+        lottoDrawingMachine);
+
+    lottoController.start();
   }
 }
